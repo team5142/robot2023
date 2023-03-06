63f4658459134daa6d518ba4ba4d8f92482e76e9
spengre <spencerscottg@gmail.com>
2023-03-05 19:58:39 -0500

    End Drivetrain 3/5

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -74,9 +74,9 @@ public class Drivetrain extends SubsystemBase {
   // }
   
 
-  public void drive(double xSpeed, double ySpeed, double rot) {
+  public void drive(double xSpeed, double ySpeed, double rot) { 
     // rotation2d = Rotation2d.fromDegrees(m_gyro.getAngle());
-    m_drive.driveCartesian(-xSpeed, ySpeed, rot);
+    m_drive.driveCartesian(-xSpeed, -ySpeed, -rot);
 
     // m_drive.driveCartesian(forward, strafe, rotation, m_gyro.getRotation2d());
   }
