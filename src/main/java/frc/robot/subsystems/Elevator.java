04af59e6883a0a4a316d2bfdc995ce0b76b74092
SystemOfParts <97854567+SystemOfParts@users.noreply.github.com>
2023-03-11 08:45:18 -0500

    Limit Switch base

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.playingwithfusion.CANVenom;
 import com.playingwithfusion.CANVenom.BrakeCoastMode;
 import com.playingwithfusion.CANVenom.ControlMode;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,6 +16,7 @@ public class Elevator extends SubsystemBase {
 
   // Define the elevator motor and encoder
   private CANVenom m_elevator;
+  DigitalInput ZeroLimitSwitch = new DigitalInput(0);
 
   // Constructor for the elevator subsystem
   public Elevator() {
@@ -30,9 +33,16 @@ public class Elevator extends SubsystemBase {
     m_elevator.set(0);
   }
   
+  public void zero() {
+    m_elevator.setCommand(ControlMode.PositionControl, 0);
+    if (ZeroLimitSwitch.get()) {
+      m_elevator.setPosition(0);
+    }
+  }
+
   public void toBottom() {
-    m_elevator.setCommand(ControlMode.PositionControl, 5);
-    // m_elevator.setPossition(5);
+    m_elevator.setCommand(ControlMode.PositionControl, 0);
+    //m_elevator.setPosition(5);
   }
 
   public void toLower() {
