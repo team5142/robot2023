156fb7934b4b5b59926944648c468f4e86d29507
SystemOfParts <97854567+SystemOfParts@users.noreply.github.com>
2023-03-19 09:55:18 -0400

    ClawThang

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -7,6 +7,10 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -14,31 +18,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Claw extends SubsystemBase {
   /** Creates a new Claw. */
-  private final CANSparkMax m_clawMotor;
+  private final TalonSRX m_clawMotor;
   private final DoubleSolenoid m_clawSolenoid;
-  private Boolean m_isClosed = false; 
+  private Boolean m_isFlipped = false;
+
   public Claw() {
-    m_clawMotor = new CANSparkMax(12, MotorType.kBrushless);
+    m_clawMotor = new TalonSRX(12);
     m_clawSolenoid = new DoubleSolenoid(6, PneumaticsModuleType.CTREPCM, 0, 3);
     close();
   }
 
-  public void setSpeed(double m_clawSpeed) {
-    m_clawMotor.set(0.5);
-
+  public void intake() {
+    m_clawMotor.set(ControlMode.PercentOutput, 0.5); // Sets motor output to +50%
   }
-  private void close() {
-    m_clawSolenoid.set(Value.kForward);
-    m_isClosed = true;
+
+  public void outtake() {
+    m_clawMotor.set(ControlMode.PercentOutput, -0.5); // Sets motor output to -50%
   }
 
   private void open() {
     m_clawSolenoid.set(Value.kReverse);
-    m_isClosed = false;
+    m_isFlipped = false;
+  }
+
+  private void close() {
+    m_clawSolenoid.set(Value.kForward);
+    m_isFlipped = true;
   }
 
   public void toggle() {
-    if (m_isClosed == true) {
+    if (m_isFlipped == true) {
       open();
     } else {
       close();
